dce05418e26c828aecccec44f8c717c637586463
piyoraik <42093052+piyoraik@users.noreply.github.com>
2021-08-25 21:28:39 +0900

    amplify add api

---
+++ src/graphql/subscriptions.ts
@@ -0,0 +1,46 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = /* GraphQL */ `
+  subscription OnCreateTodo {
+    onCreateTodo {
+      id
+      name
+      staus
+      deadLine
+      description
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onUpdateTodo = /* GraphQL */ `
+  subscription OnUpdateTodo {
+    onUpdateTodo {
+      id
+      name
+      staus
+      deadLine
+      description
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onDeleteTodo = /* GraphQL */ `
+  subscription OnDeleteTodo {
+    onDeleteTodo {
+      id
+      name
+      staus
+      deadLine
+      description
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
